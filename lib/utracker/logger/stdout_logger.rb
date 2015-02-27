82d0770920061a19a88a5264bfa21b4a6320f850
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-02-27 13:55:14 +0100

    Make the StdoutLogger output JSON.

--- lib/utracker/logger/stdout_logger.rb
+++ lib/utracker/logger/stdout_logger.rb
@@ -5,21 +5,14 @@ class Utracker::StdoutLogger < Utracker::Logger
   protected
 
   def write(event)
-    puts [
-      event.datetime.to_s,
-      event.service,
-      event.description,
-      event.message.uuid,
-      serialized_content(event.message.content),
-    ].join(' ; ')
-  end
-
-  def serialized_content(content)
-    case content
-    when String then content
-    when Hash, Array then MultiJson.dump(content)
-    else fail "Message content of type '#{content.class}' are not supported."
-    end
+    STDOUT.puts MultiJson.dump({
+      datetime: event.datetime.to_s,
+      service: event.service,
+      description: event.description,
+      uuid: event.message.uuid,
+      parent_uuid: event.message.parent_uuid,
+      payload: event.message.content,
+    })
   end
 
 end
