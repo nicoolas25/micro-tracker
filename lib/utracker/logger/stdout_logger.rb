b7f1434734bf5358e6a788c4aebc0d5b06c59f9e
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-02-27 12:37:04 +0100

    Untested dump of the message payload.

--- lib/utracker/logger/stdout_logger.rb
+++ lib/utracker/logger/stdout_logger.rb
@@ -1,3 +1,5 @@
+require 'multi_json'
+
 class Utracker::StdoutLogger < Utracker::Logger
 
   protected
@@ -8,7 +10,16 @@ class Utracker::StdoutLogger < Utracker::Logger
       event.service,
       event.description,
       event.message.uuid,
+      serialized_content(event.message.content),
     ].join(' ; ')
+  end
+
+  def serialized_content(content)
+    case content
+    when String then content
+    when Hash, Array then MultiJson.dump(content)
+    else fail "Message content of type '#{content.class}' are not supported."
+    end
   end
 
 end
