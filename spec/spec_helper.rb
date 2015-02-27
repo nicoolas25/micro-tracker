2c50973ff3392b2d513ce6a627642e3477eb0e78
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-02-27 09:20:31 +0100

    Add development tools.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  # See: http://www.rubydoc.info/github/rspec/rspec-core/RSpec/Core/Configuration#disable_monkey_patching%21-instance_method
+  config.disable_monkey_patching!
+
+  config.warnings = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.profile_examples = 5
+
+  config.order = :random
+  Kernel.srand config.seed
+end
