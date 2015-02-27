43c0248518815a9dea89da872930fe5bc0c4f841
Nicolas ZERMATI <nicoolas25@gmail.com>
2015-02-27 10:56:30 +0100

    Add Utracker.configure and Utracker.config.

--- lib/utracker.rb
+++ lib/utracker.rb
@@ -4,7 +4,25 @@ module Utracker
   autoload :Message, 'utracker/message'
 
   module ModuleMethods
+    def config
+      @config ||= empty_configuration.freeze
+    end
+
+    def configure
+      @config = empty_configuration
+      yield @config
+      @config.freeze
+    end
+
     def logger
+    end
+
+    private
+
+    def empty_configuration
+      Hash.new do |_, key|
+        fail "Missing key '#{key}' in Utracker's configuration."
+      end
     end
   end
 
---
+++ spec/utracker_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe Utracker do
+  let(:config) { Utracker.config }
+
+  describe ".config" do
+    subject { config }
+    it { is_expected.to be_a Hash }
+    it { is_expected.to be_frozen }
+  end
+
+  describe ".configure" do
+    before { Utracker.configure(&block) }
+
+    describe "the resulting config[:key] value" do
+      subject { config[:key] }
+
+      context "when the value has been defined in the configure's block" do
+        let(:block) { proc do |hash| hash[:key] = :value end }
+        it { is_expected.to eq :value }
+      end
+
+      context "when the value has not been defined in the configure's block" do
+        let(:block) { proc do |hash| hash[:other_key] = :value end }
+        it { expect{ subject }.to raise_error }
+      end
+    end
+  end
+end
